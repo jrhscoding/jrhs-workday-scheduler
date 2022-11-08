7fdfa98edd1c53b8f65057806014d894207f3102
joseph <j.hernandez.92189@gmail.com>
2022-11-07 17:27:36 -0800

    added comments for functionality

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
+// allows document to load properly
 $(document).ready(function () {
 
     var currentTime = function () {
         var currentHour = moment().hours();
         console.log(currentHour);
 
+        // Checks times and updates classes accordingly
         $('.time-block').each(function () {
             var plannerHour = parseInt($(this).attr("id"));
             if (plannerHour < currentHour) {
@@ -20,6 +22,7 @@ $(document).ready(function () {
 
     };
 
+    // Save functionality to update text information and store in localStorage
     $(".saveBtn").on('click', function () {
         var textInput = $(this).siblings('.description').val();
         var timeStamp = $(this).parent().attr('id');
@@ -27,6 +30,7 @@ $(document).ready(function () {
         alert('item saved')
     });
 
+    // Retrieves textarea from local storage and sets the value accordingly
     $('#9 .description').val(localStorage.getItem("9"));
     $('#10 .description').val(localStorage.getItem("10"));
     $('#11 .description').val(localStorage.getItem("11"));
@@ -37,8 +41,10 @@ $(document).ready(function () {
     $('#16 .description').val(localStorage.getItem("16"));
     $('#17 .description').val(localStorage.getItem("17"));
 
+    // Call currentTime on load and checkTime is called every 10 seconds
     currentTime();
     var checkTime = setInterval(currentTime, 10000);
+    checkTime;
 
     $('#currentDay').text(moment().format("dddd, MMMM Do, YYYY, h:mm:ss a"));
 
