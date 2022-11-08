9415bf4b5f1b392b2961671d1d5a9cdc5f9c3078
joseph <j.hernandez.92189@gmail.com>
2022-11-07 17:22:57 -0800

    added getItems for local storage

--- assets/script.js
+++ assets/script.js
@@ -27,6 +27,16 @@ $(document).ready(function () {
         alert('item saved')
     });
 
+    $('#9 .description').val(localStorage.getItem("9"));
+    $('#10 .description').val(localStorage.getItem("10"));
+    $('#11 .description').val(localStorage.getItem("11"));
+    $('#12 .description').val(localStorage.getItem("12"));
+    $('#13 .description').val(localStorage.getItem("13"));
+    $('#14 .description').val(localStorage.getItem("14"));
+    $('#15 .description').val(localStorage.getItem("15"));
+    $('#16 .description').val(localStorage.getItem("16"));
+    $('#17 .description').val(localStorage.getItem("17"));
+
     currentTime();
     var checkTime = setInterval(currentTime, 10000);
 
